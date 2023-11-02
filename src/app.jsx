008396a674568d0aeb47bc9ebb0a4db170a67d74
joaogabriel-fn <joaogabriel.farias@hotmail.com>
2023-11-02 10:51:17 -0300

    Header and form section added

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,21 @@
-const App = () => <h1>Boiilerplate Vite + React</h1>;
+const App = () => (
+  <>
+    <header>
+      <img src="logo-espaco-mulher.png" alt="Logo Espaço Mulher" />
+      <h1>Espaço Mulher</h1>
+    </header>
+
+    <section className="form-container">
+      <form className="add-form">
+        <label>O que você precisa guardar?</label>
+        <select>
+          <option value="1">1</option>
+        </select>
+        <input placeholder="Manda aqui" />
+        <button>Adicionar</button>
+      </form>
+    </section>
+  </>
+);
 
 export { App };
